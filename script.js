86de1aae2b33d07d3bf12c65d2d736143b5b93b2
Marcelo Pereira Bastos <marcelo.jedda@gmail.com>
2023-01-06 15:27:05 -0300

    adicionado busca de filmes

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 const moviesElement = document.querySelector('.movies');
 const btnPrevElement = document.querySelector('.btn-prev');
 const btnNextElement = document.querySelector('.btn-next');
-const URL_FILMES_INICIAL = 'https://tmdb-proxy.cubos-academy.workers.dev/3/discover/movie?language=pt-BR&include_adult=false'
+const inputElement = document.querySelector('.input');
+
+const URL_INICIAL_FILMES = 'https://tmdb-proxy.cubos-academy.workers.dev/3/discover/movie?language=pt-BR&include_adult=false'
+const URL_BUSCA_FILMES = `https://tmdb-proxy.cubos-academy.workers.dev/3/search/movie?language=pt-BR&include\_adult=false`
 let paginaAtual = 0;
 let paginas = [];
 
 btnPrevElement.addEventListener('click', () => {
   voltarPagina();
   popularFilmes(paginas[paginaAtual]);
-})
+});
 
 btnNextElement.addEventListener('click', () => {
   avancarPagina();
   popularFilmes(paginas[paginaAtual]);
-})
+});
 
-fetch(URL_FILMES_INICIAL).then( async (response) => {
+inputElement.addEventListener('keydown', (ev) => {
+  const { code } = ev;
+  if(code === 'Enter'){
+    const busca = ev.target.value.trim();
+    if(busca) {
+      buscarFilme(busca);
+    } else {
+      fetch(URL_INICIAL_FILMES).then( async (response) => {
+        const data = await response.json();
+        const filmes = data.results.slice(0, 19);
+        paginas = [filmes.slice(0, 6), filmes.slice(6, 12), filmes.slice(12, 18)];
+        popularFilmes(paginas[0]);
+      });
+    }
+    inputElement.value = '';
+  }
+});
+fetch(URL_INICIAL_FILMES).then( async (response) => {
   const data = await response.json();
   const filmes = data.results.slice(0, 19);
   paginas = [filmes.slice(0, 6), filmes.slice(6, 12), filmes.slice(12, 18)];
   popularFilmes(paginas[0]);
-})
+});
 
 const criarFilme = (filme) => {
   const movie = document.createElement('div');
@@ -70,4 +90,12 @@ const voltarPagina = () => {
   } else {
     paginaAtual -= 1;
   }
+}
+
+const buscarFilme = async (busca) => {
+  const resposta = await fetch(`${URL_BUSCA_FILMES}&query=${busca}`);
+  const data = await resposta.json();
+  const filmes = data.results.slice(0, 19);
+  paginas = [filmes.slice(0, 6), filmes.slice(6, 12), filmes.slice(12, 18)];
+  popularFilmes(paginas[0]);
 }
